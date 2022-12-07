e9f333d86f1acdfec879cb0cb227bba11b3227fd
Tomas Piccinini <tomypiccinini1@gmail.com>
2022-12-06 21:37:16 -0300

    change background

--- Frontend/barter-match-frontend/src/pages/NuevaPublicacion/styled.js
+++ Frontend/barter-match-frontend/src/pages/NuevaPublicacion/styled.js
@@ -2,10 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
-  color: #FFFFFF;
-  background-image:url('https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80');
-  background-size:cover;
+  background: rgba(0, 0, 0, 0.5);
   height: 100vh;
 `
 
--- Frontend/barter-match-frontend/src/pages/Perfil/styled.js
+++ Frontend/barter-match-frontend/src/pages/Perfil/styled.js
@@ -2,13 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: ${(props) => (props.isMatch ? "100%" : "100vh")};
-  background:url('https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80');
-  background-repeat:no-repeat;
-  background-size:cover;
-  @media (max-width:960px){
-    height:100%;
-    background-repeat:repeat;
-  }
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
 `
 
